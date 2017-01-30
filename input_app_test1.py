bb362cd1bfbf7e5c147e7a312fb3e97445ff14af
johnsayeau <johnsayeau@gmail.com>
2017-01-30 12:54:26 -0700

    hi

--- input_app_test1.py
+++ input_app_test1.py
@@ -4,7 +4,7 @@ from time import sleep
 from PIL import ImageChops
 from PIL import Image
 import math, operator
-
+from uiautomator_test import *
 
 def start_input_app():
     # launch input app and go to the 4 tiles
@@ -12,22 +12,10 @@ def start_input_app():
     sleep(5)
     os.system('adb shell am start -n "com.smart.videoinput/com.smarttech.videoinput.VideoSelectActivity"')
 
-def crop_out_center(img_to_crop):
-    img = Image.open(img_to_crop)
-    width, height = img.size
-    new_width = width / 2
-    new_height = height / 2
-    left = (width - new_width) / 2
-    top = (height - new_height) / 2
-    right = (width + new_width) / 2
-    bottom = (height + new_height) / 2
-    return img.crop((left, top, right, bottom))
 
-def images_equal(img_file1, img_file2):
-    h1 = Image.open(img_file1).histogram()
-    h2 = Image.open(img_file2).histogram()
-    return math.sqrt(reduce(operator.add,
-    map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+
+
+
 
 
 
@@ -41,5 +29,6 @@ def images_equal(img_file1, img_file2):
 #bsq.screenshot("sheep2.png")
 
 
-
-print images_equal("pic3.png", "pic4.png")
+bsq = Device()
+setup_reference_pics()
+print tile_has_thumbnail(top_left_tile_bounds, bsq)
--- uiautomator_test.py
+++ uiautomator_test.py
@@ -1,9 +1,6 @@
 from uiautomator import Device
 from PIL import Image
 
-bsq = Device()
-hdmi1_textView = bsq(text="HDMI 1")
-print hdmi1_textView.info['bounds']
 
 #boundries of four tiles in input app
 top_left_tile_bounds = (165, 60, 960, 510)
@@ -11,16 +8,57 @@ top_right_tile_bounds = (1020, 60, 1815, 510)
 bottom_left_tile_bounds = (165, 570, 960, 1020)
 bottom_right_tile_bounds = (1020, 570, 1815, 1020)
 
-def screencap_tile(bounds):
+def screencap_tile(tile_bounds, bsq):
     #returns image object - not file
     #arg is the left, top, right and bottom of the area
-    whole_screen_img = Image.open(bsq.screenshot("bsq.png"))
-    return whole_screen_img.crop(bounds)
+    whole_screen_img = Image.open(bsq.screenshot("reference_pics/video_input_tiles.png"))
+    return whole_screen_img.crop(tile_bounds)
 
 def get_tile_reference_screenshots():
-    screencap_tile(top_right_tile_bounds).save("top_right_tile_ref.png")
-    screencap_tile(top_left_tile_bounds).save("top_left_tile_ref.png")
-    screencap_tile(bottom_left_tile_bounds).save("bottom_left_tile_ref.png")
-    screencap_tile(bottom_right_tile_bounds).save("bottom_right_tile_ref.png")
+    #has to be run while in the video input app with 4 tiles showing.
+    screencap_tile(top_right_tile_bounds).save("reference_pics/top_right_tile_ref.png")
+    screencap_tile(top_left_tile_bounds).save("reference_pics/top_left_tile_ref.png")
+    screencap_tile(bottom_left_tile_bounds).save("reference_pics/bottom_left_tile_ref.png")
+    screencap_tile(bottom_right_tile_bounds).save("reference_pics/bottom_right_tile_ref.png")
 
-get_tile_reference_screenshots()
+def crop_out_center(img_to_crop):
+    img = Image.open(img_to_crop)
+    width, height = img.size
+    new_width = width / 2
+    new_height = height / 2
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
+    return img.crop((left, top, right, bottom))
+
+def images_equal(img_file1, img_file2):
+    h1 = Image.open(img_file1).histogram()
+    h2 = Image.open(img_file2).histogram()
+    result = math.sqrt(reduce(operator.add,
+    map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+    if result is None:
+        return 0
+    else:
+        return result
+
+def tiles_show_usb_connection(bsq_device):
+    #setup - all tiles should have usb connected
+    if len(bsq_device(text="Touch enabled")) == 4:
+        return True
+    else:
+        return False
+
+def tile_has_thumbnail(tile_bounds, ref_tile_img_file,bsq):
+    whole_screen_img = Image.open(bsq.screenshot("temp_images/video_input_tiles.png"))
+    thumbnail_image = whole_screen_img.crop(tile_bounds)
+    thumbnail_image_cropped = crop_out_center(thumbnail_image)
+    cropped_ref_img = crop_out_center(ref_tile_img_file)
+    return images_equal(thumbnail_image_cropped, cropped_ref_img)
+
+
+
+
+def setup_reference_pics():
+    get_tile_reference_screenshots()
+
