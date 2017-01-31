c5373867933a1e05cc786783455c875c0473ffc9
johnsayeau <johnsayeau@gmail.com>
2017-01-30 17:02:34 -0700

    hi

--- input_app_test1.py
+++ input_app_test1.py
@@ -12,9 +12,30 @@ def start_input_app():
     sleep(5)
     os.system('adb shell am start -n "com.smart.videoinput/com.smarttech.videoinput.VideoSelectActivity"')
 
+def test_all_tiles_4_thumbnails(bsq):
+    start_input_app()
+    sleep(30)
+    tile_tuples = [(top_left_tile_bounds, "reference_pics/top_left_tile_ref.png"),
+                       (top_right_tile_bounds, "reference_pics/top_right_tile_ref.png"),
+                       (bottom_left_tile_bounds, "reference_pics/bottom_left_tile_ref.png")]
+    for tile in tile_tuples:
+        sleep(5)
+        match = tile_has_thumbnail(tile[0], tile[1], bsq)
+        if  match < 100:
+            print "thumbnail loaded " + str(match)
+        else:
+            print "fail"
+            print match
+            bsq.screenshot("fail_pics/fail.png")
+            exit()
 
-
-
+bsq=Device()
+#setup_reference_pics(bsq)
+for i in range(1,100):
+    print "start of iteration " + str(i)
+    test_all_tiles_4_thumbnails(bsq)
+    os.system("adb shell am broadcast -a com.android.systemui.channels.RUN_CHANNEL --es package_name com.android.settings")
+    sleep(5)
 
 
 
@@ -28,7 +49,9 @@ def start_input_app():
 #sleep(9)
 #bsq.screenshot("sheep2.png")
 
+#start_input_app()
+#sleep(30)
+#bsq = Device()
+#setup_reference_pics(bsq)
+#tile_has_thumbnail(top_left_tile_bounds,"reference_pics/top_left_tile_ref.png", bsq)
 
-bsq = Device()
-setup_reference_pics()
-print tile_has_thumbnail(top_left_tile_bounds, bsq)
