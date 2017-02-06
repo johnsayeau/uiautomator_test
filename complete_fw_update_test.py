e432aa08f33bbff6dfe15280487652af70b2fe52
johnsayeau <johnsayeau@gmail.com>
2017-02-06 14:36:40 -0700

    o

--- complete_fw_update_test.py
+++ complete_fw_update_test.py
@@ -1,5 +1,6 @@
 from uiautomator import Device
 from subprocess import Popen
+import subprocess
 import commands
 import os
 from new_connect_usb import UsbConnection
@@ -29,8 +30,7 @@ def disconnect_usb_stick(usb_con):
     usb.disconnect_usb(usb_con)
 
 def save_logcat_to_file(file_name):
-    cmd = 'adb logcat >> ' + file_name
-    p = Popen(['adb logcat >> '])
+    p = Popen('adb logcat >> fail_file.txt', stdout=subprocess.PIPE, shell=True)
     sleep(8)
     p.kill()
 
@@ -46,22 +46,41 @@ def wait_for_button_and_click(bsq, button_text):
         raise Exception("canna find the button")
 
 def check_for_spinner(bsq):
+    ## fix this
     for i in range (1,11):
         if bsq(className="android.WidgetProgressBar").exists:
             print "spinner present"
-            sleep(1)
+            return
+        sleep(5)
     if not bsq(className="android.WidgetProgressBar").exists:
         print "aaaaw krrrap I canna find no spinner"
 
+def get_MCU_version():
+    #launch scaler settings
+    os.system("adb shell am broadcast -a com.android.systemui.channels.RUN_CHANNEL --es package_name 'com.android.settings' > /dev/null 2>&1")
+    bsq = Device()
+    bsq(text="About board").click()
+    list_view = bsq(className="android.widget.ListView")
+    return list_view.child(className="android.widget.LinearLayout",instance=5).child(resourceId="android:id/summary").info['text']
+
+def get_scaler_build_number():
+    # launch scaler settings
+    os.system("adb shell am broadcast -a com.android.systemui.channels.RUN_CHANNEL --es package_name 'com.android.settings' > /dev/null 2>&1")
+    bsq = Device()
+    bsq(text='About board').click()
+    bsq(scrollable=True).scroll.to(text="Build number")
+    return bsq(text="Build number").sibling(resourceId="android:id/summary").info['text']
+    return bsq(className="android.widget.RelativeLayout",instance=9).child(resourceId="android:id/summary").info['text']
 
 def update_fw(usb_stick_connection):
     connect_usb_stick(usb_stick_connection)
     bsq = connect_device("10.1.108.81")
     wait_for_button_and_click(bsq, "Restart")
-    check_for_spinner(bsq)
+    #check_for_spinner(bsq)
     sleep(600)  # wait for 600 seconds for fw update to complete
     disconnect_usb_stick(usb_stick_connection)
     sleep(3)
+
 
 
 
@@ -70,7 +89,7 @@ run = 1
 while True:
     update_fw("usb5")
     update_fw("usb6")
-    print "run # " + str(1)
+    print "run # " + str(run)
     run +=1
 
 
